e1029aba38e9592723d95ae9cc312f62fa078f71
Dennis Willie <denniswillie2000@gmail.com>
2022-11-21 16:20:39 +0000

    Created UserModel, which represents the schema for the User table in the database

---
+++ backend/db/models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    googleId: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
