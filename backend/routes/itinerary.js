71320f13e739649572df65f32fe97e5f416e57aa
Dennis Willie <denniswillie2000@gmail.com>
2022-11-23 16:15:48 +0000

    Homepage endpoint: itineraries startsWith

---
+++ backend/routes/itinerary.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const ItineraryManager = require('../db_managers/ItineraryManager');
+
+router.post('/namestartsWith', async (req, res) => {
+    const starting = req.body.starting;
+    try {
+        const itineraries = await ItineraryManager.
+        res.send({
+            statusCode: 200,
+            itineraries: itineraries, 
+        });
+    } catch (error) {
+        res.send({
+            statusCode: 500,
+            itineraries: [],
+        });
+    }
+});
