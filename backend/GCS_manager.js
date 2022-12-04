a5ee9c0afa720d3138c75945339a87e9544650a1
Jeremy Neo <doremineo@gmail.com>
2022-12-04 14:55:56 +0000

    Created a GCS function to upload audio file

---
+++ backend/GCS_manager.js
@@ -0,0 +1,31 @@
+const { Storage } = require("@google-cloud/storage");
+const path = require("path");
+const gc = new Storage({
+  keyFilename: path.join(
+    __dirname,
+    "../loyal-venture-368318-8a7920f2df96.json"
+  ),
+  projectId: "loyal-venture-368318",
+});
+const bucket = gc.bucket("guidify_bucket");
+
+class GCSManager {
+  static async uploadAudio(audio, id) {
+    await new Promise((resolve, reject) => {
+      const blob = bucket.file(file);
+      blob.name = `${id},${audio.mimetype.split("/")[1]}`;
+      const blobStream = blob.createWriteStream();
+      blobStream
+        .on("finish", () => {
+          const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+          resolve(publicUrl);
+        })
+        .on("error", () => {
+          reject(`Unable to upload image, something went wrong`);
+        })
+        .end(audio.buffer);
+    });
+  }
+}
+
+module.exports = GCSManager;
