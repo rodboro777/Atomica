af4032e0fbec5d9e1d2d355696ac0ae69e41ca04
Dennis Willie <denniswillie2000@gmail.com>
2022-12-05 20:27:38 +0000

    Added a function to fetch travel guides from database based on the given palce id

--- backend/db_managers/travelGuideManager.js
+++ backend/db_managers/travelGuideManager.js
@@ -4,6 +4,10 @@ const TravelGuideModel = require("../models/travelGuideModel");
 
 class TravelGuideManager {
     
+    static async getTravelGuidesByPlaceId(placeId) {
+        return [];
+    }
+
 }
 
 module.exports = TravelGuideManager;
