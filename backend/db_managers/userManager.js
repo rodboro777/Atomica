e25be6a807be9187c8229812b65ef91957869b87
Dennis Willie <denniswillie2000@gmail.com>
2023-02-06 10:54:28 +0000

    create endpoint to retrieve user username

--- backend/db_managers/userManager.js
+++ backend/db_managers/userManager.js
@@ -3,7 +3,10 @@
 const UserModel = require("../models/userModel");
 
 class UserManager {
-    
+    static async getUsernameById(id) {
+        const doc = await UserModel.findById(id);
+        return doc.username;
+    }
 }
 
 module.exports = UserManager;
