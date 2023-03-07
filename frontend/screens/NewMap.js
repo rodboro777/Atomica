3becc5d40533c523f4c744a7d1dd2d1397ccd266
Dennis Willie <denniswillie2000@gmail.com>
2023-03-07 03:11:10 +0000

    show road labels on maps

--- frontend/screens/NewMap.js
+++ frontend/screens/NewMap.js
@@ -952,16 +952,12 @@ const styles = StyleSheet.create({
 
 const mapStyle = [
   {
-    featureType: 'all',
-    elementType: 'labels.text',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{color: 'black'}],
   },
   {
-    featureType: 'all',
+    featureType: 'poi',
     elementType: 'labels.icon',
     stylers: [
       {
